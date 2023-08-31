f37afe1fb195a906f4355777a09c49cda3c5ea03
Luis Polina <luis.282001@gmail.com>
2023-08-30 21:37:19 -0600

    Sección 2 - Commit inicial API

---
+++ CitasApp.Service/Data/DataContext.cs
@@ -0,0 +1,13 @@
+
+using CitasApp.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitasApp.Service.Data;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set;}
+}
