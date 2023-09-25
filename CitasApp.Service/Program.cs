6706120fac147c257920d356bc4285d9071e922d
P4R-D4RK <luis.282001@gmail.com>
2023-09-24 18:41:38 -0600

    Config Swagger

--- CitasApp.Service/Program.cs
+++ CitasApp.Service/Program.cs
@@ -10,9 +10,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
